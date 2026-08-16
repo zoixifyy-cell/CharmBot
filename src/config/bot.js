84be3097f1b96ca46b78b903d92faf77fb71c031
zoixifyy-cell <zoixifyy@gmail.com>
2026-08-15 22:42:32 -0700

    Change footer text from 'Titan Bot' to 'Cure Bot'

--- src/config/bot.js
+++ src/config/bot.js
@@ -141,7 +141,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Titan Bot",
+      text: "Cure Bot",
       // Footer icon URL (null = no icon).
       icon: null,
     },
