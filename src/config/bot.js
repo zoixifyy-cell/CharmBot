6402f4fd568fbc4ff5afa232a746eacf07c4fe3d
zoixifyy-cell <zoixifyy@gmail.com>
2026-08-15 23:49:00 -0700

    Update bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Watching Countdown", // required by Discord API, not shown in the client
+        name: "Custom Status", // required by Discord API, not shown in the client
         state: "www.remedie.us",     // this is what people actually see
-        type: 3,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
