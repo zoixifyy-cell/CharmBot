73b9a8a2919606a13f19d5f51a70952e4cf3c892
zoixifyy-cell <zoixifyy@gmail.com>
2026-09-12 18:44:45 -0700

    Update bot custom status URL

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "discord.gg/zoix",     // this is what people actually see
+        state: "discord.gg/glimmer ✮",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
